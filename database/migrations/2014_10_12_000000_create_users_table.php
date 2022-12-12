e284dce687fc605eae29dd140783f9674e05dc40
joshua-jojo <joshuasitompul2000@gmail.com>
2022-12-12 13:26:07 +0700

    update data user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->date('tanggal_lahir');
+            $table->text('alamat');
             $table->string('password');
             $table->integer('perusahaan_id')->default(0);
             $table->string('theme')->default('winter');
